d8d614cdedfd5123ffd8ac5bca608412215d13fa
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2023-07-28 00:47:56 -0400

    dummy login

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String correctUsername = "mustafa";
+        String correctPassword = "123";
+        System.out.print("Enter your username: ");
+        String inputUsername = scanner.nextLine();
+        System.out.print("Enter your password: ");
+        String inputPassword = scanner.nextLine();
+        if (correctUsername.equals(inputUsername) && correctPassword.equals(inputPassword)) {
+            System.out.println("Login successful!");
+        } else {
+            System.out.println("Login failed. Invalid username or password.");
+        }
+
+        scanner.close();
     }
 }
