590751cecf1402263864ac020fb9f1acd4bcf1df
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2023-07-28 20:19:56 -0400

    sign up is wokring

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,64 @@
 import java.sql.*;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
         String url = "jdbc:mysql://127.0.0.1/C43";
         String username = "root";
         String password = "Mustafa0503";
 
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
-
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Enter your username: ");
-            String inputUsername = scanner.nextLine();
+            System.out.println("Select an option:");
+            System.out.println("1. Log in");
+            System.out.println("2. Sign up");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
 
-            System.out.print("Enter your password: ");
-            String inputPassword = scanner.nextLine();
+            if (choice == 1) {
+                // Log in
+                System.out.print("Enter your username: ");
+                String inputUsername = scanner.next();
 
-            String selectQuery = "SELECT * FROM users WHERE username = ? AND password = ?";
-            PreparedStatement preparedStatement = connection.prepareStatement(selectQuery);
-            preparedStatement.setString(1, inputUsername);
-            preparedStatement.setString(2, inputPassword);
+                System.out.print("Enter your password: ");
+                String inputPassword = scanner.next();
 
-            ResultSet resultSet = preparedStatement.executeQuery();
+                if (loginUser(connection, inputUsername, inputPassword)) {
+                    System.out.println("Login successful!");
+                } else {
+                    System.out.println("Login failed. Invalid username or password.");
+                }
 
-            if (resultSet.next()) {
-                System.out.println("Login successful!");
+            } else if (choice == 2) {
+                // Sign upgit
+                SignUp.performSignUp(connection, scanner);
             } else {
-                System.out.println("Login failed. Invalid username or password.");
+                System.out.println("Invalid choice. Please select 1 or 2.");
             }
 
             // Close resources
-            resultSet.close();
-            preparedStatement.close();
             scanner.close();
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean loginUser(Connection connection, String username, String password) throws SQLException {
+        String selectQuery = "SELECT * FROM users WHERE username = ? AND password = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(selectQuery);
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        boolean loginSuccessful = resultSet.next();
+
+        resultSet.close();
+        preparedStatement.close();
+
+        return loginSuccessful;
     }
 }
--- src/SignUp.java
+++ src/SignUp.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Signup {
+import java.sql.*;
+import java.util.Scanner;
+public class SignUp {
+    public static void performSignUp(Connection connection, Scanner scanner) throws SQLException {
+        System.out.print("Enter a new username: ");
+        String newUsername = scanner.next();
+
+        System.out.print("Enter a new password: ");
+        String newPassword = scanner.next();
+
+        String insertQuery = "INSERT INTO users (username, password) VALUES (?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+        preparedStatement.setString(1, newUsername);
+        preparedStatement.setString(2, newPassword);
+
+        int rowsAffected = preparedStatement.executeUpdate();
+
+        if (rowsAffected > 0) {
+            System.out.println("Sign up successful! You can now log in.");
+        } else {
+            System.out.println("Sign up failed. Please try again.");
+        }
+
+        preparedStatement.close();
+    }
 }
