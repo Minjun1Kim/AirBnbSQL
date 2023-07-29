5babc9ddbc38a7f724d9eb88b0907c392db0035a
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2023-07-29 14:33:18 -0400

    user can login and sign up as admin or regular uer

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,13 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
 
             System.out.println("Select an option:");
-            System.out.println("1. Log in");
-            System.out.println("2. Sign up");
+            System.out.println("1. Log in as user");
+            System.out.println("2. Log in as admin");
+            System.out.println("3. Sign up");
             System.out.print("Enter your choice: ");
             int choice = scanner.nextInt();
 
-            if (choice == 1) {
+            if (choice == 1 || choice == 2) {
                 // Log in
                 System.out.print("Enter your username: ");
                 String inputUsername = scanner.next();
@@ -25,17 +26,31 @@ public class Main {
                 System.out.print("Enter your password: ");
                 String inputPassword = scanner.next();
 
-                if (loginUser(connection, inputUsername, inputPassword)) {
+                boolean isUserLogin = choice == 1;
+                boolean loginSuccessful = loginUser(connection, inputUsername, inputPassword, isUserLogin);
+
+                if (loginSuccessful) {
                     System.out.println("Login successful!");
+
+                    if (isUserLogin) {
+                        System.out.println("hello, user!");
+                    } else {
+                        // Check if the user is an admin in the database
+                        if (isAdminUser(connection, inputUsername)) {
+                            System.out.println("Welcome, admin!");
+                        } else {
+                            System.out.println("You do not have authentication as admin.");
+                        }
+                    }
                 } else {
                     System.out.println("Login failed. Invalid username or password.");
                 }
 
-            } else if (choice == 2) {
-                // Sign upgit
+            } else if (choice == 3) {
+                // Sign up
                 SignUp.performSignUp(connection, scanner);
             } else {
-                System.out.println("Invalid choice. Please select 1 or 2.");
+                System.out.println("Invalid choice. Please select 1, 2, or 3.");
             }
 
             // Close resources
@@ -46,11 +61,12 @@ public class Main {
         }
     }
 
-    private static boolean loginUser(Connection connection, String username, String password) throws SQLException {
-        String selectQuery = "SELECT * FROM users WHERE username = ? AND password = ?";
+    private static boolean loginUser(Connection connection, String username, String password, boolean isUserLogin) throws SQLException {
+        String selectQuery = "SELECT * FROM users WHERE username = ? AND password = ? AND admin = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(selectQuery);
         preparedStatement.setString(1, username);
         preparedStatement.setString(2, password);
+        preparedStatement.setBoolean(3, !isUserLogin);
 
         ResultSet resultSet = preparedStatement.executeQuery();
 
@@ -60,5 +76,20 @@ public class Main {
         preparedStatement.close();
 
         return loginSuccessful;
+    }
+
+    private static boolean isAdminUser(Connection connection, String username) throws SQLException {
+        String selectQuery = "SELECT admin FROM users WHERE username = ? AND admin = true";
+        PreparedStatement preparedStatement = connection.prepareStatement(selectQuery);
+        preparedStatement.setString(1, username);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        boolean isAdmin = resultSet.next();
+
+        resultSet.close();
+        preparedStatement.close();
+
+        return isAdmin;
     }
 }
--- src/SignUp.java
+++ src/SignUp.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 import java.util.Scanner;
+
 public class SignUp {
     public static void performSignUp(Connection connection, Scanner scanner) throws SQLException {
         System.out.print("Enter a new username: ");
@@ -8,10 +9,15 @@ public class SignUp {
         System.out.print("Enter a new password: ");
         String newPassword = scanner.next();
 
-        String insertQuery = "INSERT INTO users (username, password) VALUES (?, ?)";
+        System.out.print("Are you registering as an admin? (y/n): ");
+        String isAdminInput = scanner.next();
+        boolean isAdmin = isAdminInput.equalsIgnoreCase("y");
+
+        String insertQuery = "INSERT INTO users (username, password, admin) VALUES (?, ?, ?)";
         PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
         preparedStatement.setString(1, newUsername);
         preparedStatement.setString(2, newPassword);
+        preparedStatement.setBoolean(3, isAdmin);
 
         int rowsAffected = preparedStatement.executeUpdate();
 
