9061c8d20c4669e30f3c2d9014b796ee91d8f8c6
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2023-08-06 00:00:36 -0400

    search, view, and create listings

---
+++ src/UserContext.java
@@ -0,0 +1,15 @@
+public class UserContext{
+    private String username;
+
+    // Add other user-related information as needed
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Add other setter and getter methods for other user-related information
+}
